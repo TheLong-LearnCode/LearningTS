49f1517d1a27c9ed8fd727ce5cc42a45612bdf99
Minh-Thong2211 <thelong.workhard@gmail.com>
2025-01-12 17:12:31 +0700

    lesson 1

---
+++ lesson1/lesson1.js
@@ -0,0 +1,4 @@
+var message = 'Hello world, TypeScript!';
+console.log(message);
+// Run: ts-node lesson1.ts
+// Output: Hello world, TypeScript!
---
+++ lesson1/lesson1.ts
@@ -0,0 +1,7 @@
+const message: string = 'Hello world, TypeScript!';
+console.log(message);
+
+// Run in terminal: ts-node lesson1.ts
+// Output: Hello world, TypeScript!
+
+// Run in browser: tsc lesson1.ts
