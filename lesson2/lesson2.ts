6bed56e8b5a01337607028abda9f522230600a1d
Minh-Thong2211 <thelong.workhard@gmail.com>
2025-01-22 21:34:55 +0700

    lesson 2 + 3

---
+++ lesson2/lesson2.ts
@@ -0,0 +1,9 @@
+const  name1 = "thong";
+
+console.log("length = ", name1.length)
+console.log("upperCase = ", name1.toUpperCase())
+
+
+// ts-node lesson2.ts //
+// Output: length = 5
+// Output: upperCase = THONG
---
+++ lesson3/lesson3.ts
@@ -0,0 +1,13 @@
+let count: number = 1;
+let name2: string = "John";
+
+
+console.log("<<<< Check count >>>>", count);
+console.log("<<<< Check name >>>>", name2);
+
+let arr: string[] = ['Thong', 'John', 'Doe', 'TypeScript'];
+arr.push('20');
+
+console.log("<<<< Check array after push >>>>", arr);
+
+
