6de5d74a0c4092479ece47d4270bf15ca8151f41
Minh-Thong2211 <thelong.workhard@gmail.com>
2025-01-31 15:06:06 +0700

    lesson4 and 5

--- lesson4/lesson4.ts
+++ lesson4/lesson4.ts
@@ -1,1 +1,9 @@
- 
+ let test: number = 9.6;
+
+//  test = "abc"; // Error: Type 'string' is not assignable to type 'number'.
+
+const test2: number = 0.4;
+// test2 = 3; // Error: Cannot assign to 'test2' because it is a constant.
+
+console.log("<<<< Check sum >>>>", test + test2);
+// console.log("<<<< Check sum >>>>", test + test2 + "abc"); // Error: Operator '+' cannot be applied to types 'number' and 'string'.
---
+++ lesson5/lesson5.ts
@@ -0,0 +1,12 @@
+let testName = `Thong Le and "Purple"`; //auto type string
+
+let testName1: string = `The couple: ${testName}`; //explicit type string
+
+let testName2 = `Thong Le and
+ "Purple"`;
+
+console.log("<<<< Check testName >>>>", testName);
+console.log("<<<< Check testName2 >>>>", testName2);
+console.log("<<<< Check testName1 >>>>", testName1);
+
+
